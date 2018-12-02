ea0d4e73f4779cba90266bbb01c3119be5a13937
sai <saicharan.reddy1@gmail.com>
2018-12-03 00:00:45 +0530

    decentralized replace chain done

--- 2a. Crytocurrency Intuition/saicoin.py
+++ 2a. Crytocurrency Intuition/saicoin.py
@@ -155,11 +155,23 @@ def connect_node():
     nodes = json.get('nodes')
     if nodes is None:
         return 'No node',400
-
     for node in nodes:
         blockchain.add_node(node)
-    response = {'message': 'All nodes are now connected and now contains the following nodes':
+    response = {'message': 'All nodes are now connected and now contains the following nodes',
     'total_nodes':list(blockchain.nodes)}
     return jsonify(response),201
+
+#replace chain
+@app.route('/replace_chain', methods = ['GET','POST'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_valid:
+        response = {'message': 'Chain has been replaced','new_chain': blockchain.chain}
+    else:
+        response = {'message': 'All good. The chain is the largest one','actual_chain':blockchain.chain}
+    return jsonify(response),200
+
+
+
 # Running the app
 app.run(host = '0.0.0.0', port = 5000)
